941cc8f9efe556f738f19e4db52801f5dd9ab573
Donny Pratama <donnietama8@gmail.com>
2018-07-16 02:03:48 +0700

    setup factory

--- database/factories/ReportSummaryFactory.php
+++ database/factories/ReportSummaryFactory.php
@@ -1,9 +1,18 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon as Carbon;
+use Auth;
 
 $factory->define(App\ReportSummary::class, function (Faker $faker) {
     return [
-        //
+        'store_id' => Auth::user()->id,
+        'date' => Carbon::now(),
+        'gross' => $faker->numberBetween($min = 30000, $max = 100000),
+        'nett' => $faker->numberBetween($min = 30000, $max = 100000),
+        'voucher' => $faker->numberBetween($min = 30000, $max = 100000),
+        'cash' => $faker->numberBetween($min = 30000, $max = 100000),
+        'card' => $faker->numberBetween($min = 30000, $max = 100000),
+        'ticket' => $faker->numberBetween($min = 30000, $max = 100000),
     ];
 });
