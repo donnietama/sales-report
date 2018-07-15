e4145e6ed24715993afd0ba2903ae77381d3d688
Donny Pratama <donnietama8@gmail.com>
2018-07-16 04:13:30 +0700

    modified submitRequest function

--- app/Http/Controllers/ReportSummaryController.php
+++ app/Http/Controllers/ReportSummaryController.php
@@ -15,30 +15,24 @@ class ReportSummaryController extends Controller
 
     public function submitReport(Request $request)
     {
-        $resource = new ReportSummary;
-        $resource->store_id = Auth::user()->id;
-        $resource->date = $request->date ?: 0;
-        $resource->gross = $request->gross ?: 0;
-        $resource->nett = $request->nett ?: 0;
-        $resource->voucher = $request->voucher ?: 0;
-        $resource->cash = $request->cash ?: 0;
-        $resource->card = $request->card ?: 0;
-        $resource->ticket = $request->ticket ?: 0;
+        $resource = ReportSummary::create([
+            'store_id' => Auth::user()->id,
+            'date' => $request->date ?: 0,
+            'gross' => $request->gross ?: 0,
+            'nett' => $request->nett ?: 0,
+            'voucher' => $request->voucher ?: 0,
+            'cash' => $request->cash ?: 0,
+            'card' => $request->card ?: 0,
+            'ticket' => $request->ticket ?: 0,
+        ]);
 
-        if ($resource->save())
-        {
-            return response()->json('Laporan selesai!');
-        }
-        else
-        {
-            return response()->json('Gagal mengirim data!');
-        }
+        return response()->json(ReportSummary::find($resource->id));
     }
 
     public function getReport()
     {
         $store_id = Auth::user()->id;
-        $data = ReportSummary::where('store_id', '=', $store_id)->get();
+        $data = ReportSummary::where('store_id', '=', $store_id)->orderBy('created_at', 'desc')->get();
 
         return response()->json($data);
     }
