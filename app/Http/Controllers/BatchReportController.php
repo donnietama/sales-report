08e8c7d3ef6af44ead53b2174cdb32dfdbca1678
Donny Pratama <donnietama8@gmail.com>
2018-07-16 13:36:38 +0700

    added Batch Reporting

---
+++ app/Http/Controllers/BatchReportController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\BatchReport;
+use Auth;
+
+class BatchReportController extends Controller
+{
+    public function index()
+    {
+        return view('batch.index');
+    }
+
+    public function submitReport(Request $request)
+    {
+        $resource = BatchReport::create([
+            'store_id' => Auth::user()->id,
+            'product_id' => $request->product_id,
+            'quantity' => $request->quantity,
+        ]);
+
+        return response()->json(BatchReport::find($resource->id));
+    }
+
+    public function getReport()
+    {
+        $id = Auth::user()->id;
+        $data = BatchReport::where('store_id', '=', $id)->orderBy('id', 'desc')->paginate(15);
+
+        return response()->json($data);
+    }
+}
