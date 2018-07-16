27159a8f7d983329e62bb7543c951e5943cf0599
Donny Pratama <donnietama8@gmail.com>
2018-07-16 15:51:03 +0700

    partitioning layouts

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 p-0 bg-dark">
+    <div class="sidebar bg-dark text-capitalize">
+        <ul>
+            <a href="{{ route('home') }}" class=" text-white">
+                <li class="bg-success p-3">
+                    Kembali ke menu awal
+                </li>
+            </a>
+            <li class="bg-secondary p-3">
+                <a href="/home/summaries/create" class="text-white">summary reporting</a>
+            </li>
+            <li class="bg-secondary p-3">
+                <a href="#" class="text-white">batch reporting</a>
+            </li>
+            <div class="bg-secondary p-3">
+                <a href="#" class="text-white">product sold reporting</a>
+            </div>
+            <div class="bg-secondary p-3">
+                <a href="#" class="text-white">additional reporting</a>
+            </div>
+        </ul>
+    </div>
+</div>
