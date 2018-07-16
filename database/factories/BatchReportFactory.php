adec3c7b4fe466873c85d6ffa38a166256197eda
Donny Pratama <donnietama8@gmail.com>
2018-07-16 14:38:19 +0700

    fixed typo

--- database/factories/BatchReportFactory.php
+++ database/factories/BatchReportFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\BatchReport::class, function (Faker $faker) {
     return [
-        'store_id' => $factory->randomDigitNotNull,
-        'product_id' => $factory->randomDigitNotNull,
+        'store_id' => $faker->randomDigitNotNull,
+        'product_id' => $faker->randomDigitNotNull,
         'quantity' => $faker->numberBetween($min = 30000, $max = 100000),
     ];
 });
